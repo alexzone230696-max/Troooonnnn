b54938b2942671dd184e48031da9727247114306
alexzone230696-max <alexzone230696@gmail.com>
2025-11-21 18:41:41 +0500

    Create pages/index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Wallet from "../components/Wallet";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Tron Wallet (TS + Vercel)</h1>
+      <Wallet />
+    </div>
+  );
+}
